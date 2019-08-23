8d3d9fc129f7afd30116125f89832fbc13ab0905
Catherine Clause <cnclause123@gmail.com>
2019-08-23 15:16:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length 
-  puts arary [counter]
+  puts array [counter]
   counter +=1 
 end
 return array.index 
